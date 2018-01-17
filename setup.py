df93bd3d4d2ccc221bfe0b27baa815306151938f
Anthony Bloomer <ant0@protonmail.ch>
2018-01-17 08:00:35 +0000

    Update

--- setup.py
+++ setup.py
@@ -1,13 +1,55 @@
-from setuptools import setup
+# -*- coding: utf-8 -*-
 
-setup(name='pyflight',
-      version='0.1',
-      description='A simple wrapper for the Google Flights Search API.',
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+setup(name='pyflights',
+      version='0.3.1',
+      description='A wrapper for the Google Flights Search API.',
       url='https://github.com/AnthonyBloomer/pyflights',
       author='Anthony Bloomer',
       author_email='ant0@protonmail.ch',
       license='MIT',
-      packages=['pyflight', 'pyflight.objects'],
+      packages=['pyflights', 'pyflights.objects'],
       keywords=['Google Flights', 'Flights', 'API', 'QPX Express Airfare API'],
       install_requires=[
           'requests',
@@ -16,6 +58,15 @@ setup(name='pyflight',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           "Topic :: Software Development :: Libraries",
-          'Programming Language :: Python :: 2.7'
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
       ],
+      cmdclass={
+          'publish': PublishCommand,
+      },
       zip_safe=False)
